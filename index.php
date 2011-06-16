61c3503d5db7c0e7e0193f0ea9140f08ab44dd14
Pradeep Gowda (on pradeepgowda.com) <pradeep@btbytes.com>
2011-06-16 03:03:19 +0000

    improved error handling

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
-$q = $_GET['q'];
-$res = exec("grep ^\"".$q."\" links.txt | cut  -f2");
-if ($res) 
-{
-   header("Location: ".$res);
-   exit;
-}
-else
+function not_found()
 {
     header("Location: /404.html");
+}
+
+function home_page()
+{
+    echo "<h1>shrt</h1>";
+}
+
+
+$q = $_GET['q'];
+if (strlen($q) > 0) 
+{
+    $res = exec("grep ^\"".$q."\" links.txt | cut  -f2");
+    if ($res) 
+    {
+       header("Location: ".$res);
+       exit;
+    }
+    else
+    {
+        not_found();
+    }
+
+} 
+else 
+{
+    home_page();
 }
 ?>
